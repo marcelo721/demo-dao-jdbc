e4a9657f971bd460b9db3fcea88681f7c0abb5de
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-04 15:52:21 -0300

    seller entity class

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,10 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Progam {
 
     public static void main(String[] args) {
-        
+
+        Department obg = new Department(1,"Books");
+        Seller seller= new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obg);
+        System.out.println(obg);
+        System.out.println(seller);
     }
 }
