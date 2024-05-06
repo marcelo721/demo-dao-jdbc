89eae468cc9e320fb5c8ce5bf797810484cae82b
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-06 09:34:38 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,7 @@ public class Progam {
 
         Department obg = new Department(1,"Books");
         Seller seller= new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obg);
-        System.out.println(obg);
-        System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 }
