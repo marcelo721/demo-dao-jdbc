a0193a14c7d5132694c5f8b93462cf98b4800326
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-06 09:59:49 -0300

    findById implementation

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -11,9 +11,9 @@ public class Progam {
 
     public static void main(String[] args) {
 
-        Department obg = new Department(1,"Books");
-        Seller seller= new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obg);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
